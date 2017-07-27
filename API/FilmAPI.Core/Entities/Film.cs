d1f0cb6bf7e63ac60dc694c6eaf00a104d89a822
Robert Switzer <sewitzer40@googlemail.com>
2017-07-27 05:07:13 +0200

    Core Finished

---
+++ API/FilmAPI.Core/Entities/Film.cs
@@ -0,0 +1,23 @@
+﻿using FilmAPI.Core.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FilmAPI.Core.Entities
+{
+    public class Film : BaseEntity
+    {
+        public Film(string title, short year, short length = 0)
+        {
+            Title = title;
+            Year = year;
+            Length = length;
+        }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public short Year { get; set; }
+        public short Length { get; set; }
+    }
+}
---
+++ API/FilmAPI.Core/Entities/Medium.cs
@@ -0,0 +1,23 @@
+﻿using FilmAPI.Core.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FilmAPI.Core.Entities
+{
+    public class Medium : BaseEntity
+    {
+        public Medium(int filmId, string type, string location = "")
+        {
+            FilmId = filmId;
+            MediumType = type;
+            Location = location;
+        }
+        [Required]
+        public int FilmId { get; set; }
+        [Required]
+        public string MediumType { get; set; }
+        public string Location { get; set; }
+    }
+}
