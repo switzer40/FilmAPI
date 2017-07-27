911831d483b38a990f001853a82f088575ca0efb
Robert Switzer <sewitzer40@googlemail.com>
2017-07-27 17:30:58 +0200

    Core Done
    Now the Vore Layer really is finisned

---
+++ API/FilmAPI.Core/Interfaces/ISpecification.cs
@@ -0,0 +1,11 @@
+﻿using FilmAPI.Core.SharedKernel;
+using System;
+using System.Linq.Expressions;
+
+namespace FilmAPI.Core.Interfaces
+{
+    public interface ISpecification<T> where T : BaseEntity
+    {
+        Expression<Func<T, bool>> Predicate { get; }
+    }
+}
