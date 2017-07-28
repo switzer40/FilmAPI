2e20b5a3a4187bad5253e68d6fb6bf693863b91a
Robert Switzer <sewitzer40@googlemail.com>
2017-07-29 01:11:29 +0200

    Infratstructure really done
    I hope those were the finishing touches on the infrastructure layerr.

---
+++ API/FilmAPI.Core/SharedKernel/FilmConstants.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FilmAPI.Core.SharedKernel
+{
+    public class FilmConstants
+
+    { 
+    // MediaTypes
+    public const string MediumType_BD = "BD";
+    public const string MediumType_DVD = "DVD";
+
+    // Roles
+    public const string Role_Actor = "Actor";
+    public const string Role_Composer = "Composer";
+    public const string Role_Director = "Director";
+    public const string Role_Writer = "Scriptwriter";
+
+    // Locations
+    public const string Location_LefDrawer = "Left Drawer";
+    public const string Location_RightDrawer = "Right Drawer";
+    public const string Location_LeftLevel1 = "Left Level1";
+    public const string Location_LeftLevel2 = "Left Level2";
+    public const string Location_LeftLevel3 = "Left Level3";
+    public const string Location_LeftLevel4 = "Left Level4";
+    public const string Location_RightLevel1 = "Right Level1";
+    public const string Location_RightLevel2 = "Right Level2";
+    public const string Location_RightLevel3 = "Right Level3";
+    public const string LocationRighftLevel4 = "Right Level4";
+    public const string Location_BDLevel1 = "BD Level1";
+    public const string Location_BDLevel2 = "BD Level2";
+    public const string Location_BDLevel3 = "BD Level3";
+    public const string Location_BDLeve41 = "BD Level";
+    public const string Location_Bookshelf1 = "Bookshelf1";
+    }
+}
