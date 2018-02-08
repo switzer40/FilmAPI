722514136c504733dc68cba1fff1dd194225a9a6
switzer40 <switzer40@googlemail.com>
2018-02-08 04:35:08 +0100

    Fixed PersonService

--- FilmAPI/Services/PersonService.cs
+++ FilmAPI/Services/PersonService.cs
@@ -1,7 +1,5 @@
-﻿using FilmAPI.Common.Constants;
-using FilmAPI.Common.DTOs;
+﻿using FilmAPI.Common.DTOs;
 using FilmAPI.Common.Interfaces;
-using FilmAPI.Common.Services;
 using FilmAPI.Common.Utilities;
 using FilmAPI.Core.Entities;
 using FilmAPI.Core.Interfaces;
@@ -9,17 +7,16 @@ using FilmAPI.Interfaces;
 using FilmAPI.Validation.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 
 namespace FilmAPI.Services
 {
     public class PersonService : BaseSevice<Person>, IPersonService
     {
-        private IPersonValidator _validator;
+        private readonly IPersonValidator _validator;
         public PersonService(IPersonRepository repo,
-                            IPersonMapper mapper,
-                            IPersonValidator validator) : base(repo, mapper)
+                             IPersonMapper mapper,
+                             IPersonValidator validator) : base(repo, mapper)
         {
             _validator = validator;
         }
@@ -28,7 +25,8 @@ namespace FilmAPI.Services
             var retVal = OperationStatus.OK;
             var b = (BasePersonDto)dto;
             var results = _validator.Validate(b);
-            IsValid = results.IsValid;            
+            IsValid = results.IsValid;
+
             var entityToAdd = _mapper.MapBack(b);
             var savedEntity = _repository.Add(entityToAdd);
             if (IsValid)
@@ -40,8 +38,9 @@ namespace FilmAPI.Services
             {
                 result = null;
                 retVal = OperationStatus.BadRequest;
+                retVal.ReasonForFailure = "Invalid input";
             }
-            return StandardResult(retVal);
+            return new OperationResult(retVal, result);
         }
 
         public override OperationResult ClearAll()
@@ -53,16 +52,11 @@ namespace FilmAPI.Services
         public override OperationResult Delete(string key)
         {
             var result = OperationStatus.OK;
-            var data = _keyService.DeconstructPersonKey(key);
-            if (data.lastName == FilmConstants.BADKEY)
-            {
-                result = OperationStatus.BadRequest;
-                result.ReasonForFailure = FilmConstants.KeyState_Invalid;
-            }
             var personToDelete = ((IPersonRepository)_repository).GetByKey(key);
             if (personToDelete == null)
             {
                 result = OperationStatus.NotFound;
+                result.ReasonForFailure = "No such person exists";
             }
             else
             {
@@ -74,20 +68,17 @@ namespace FilmAPI.Services
         public override OperationResult GetByKey(string key)
         {
             var data = _keyService.DeconstructPersonKey(key);
-            var dto = new KeyedPersonDto(data.lastName, data.birthdate);
-            dto.Key = key;
-            result.Add(dto);
+            var p = GetByLastNameAndBirthdate(data.lastName, data.birthdate);
+            result.Add(p);
             return new OperationResult(OperationStatus.OK, result);
         }
 
         public KeyedPersonDto GetByLastNameAndBirthdate(string lastName, string birthdate)
         {
             var p = ((IPersonRepository)_repository).GetByLastNameAndBirthdate(lastName, birthdate);
-            var key = _keyService.ConstructPersonKey(p.LastName, p.BirthdateString);
+            var key = _keyService.ConstructPersonKey(lastName, birthdate);
             return new KeyedPersonDto(p.LastName, p.BirthdateString, p.FirstMidName, key);
         }
-
-        
 
         public override OperationResult Update(IBaseDto dto)
         {
@@ -96,12 +87,14 @@ namespace FilmAPI.Services
             if (b == null)
             {
                 result = OperationStatus.BadRequest;
+                result.ReasonForFailure = "Wrong input type";
             }
             var personToUpdate = _mapper.MapBack(b);
-            var storedPerson = RetrieveStoredEntity(dto);
-            if (storedPerson == null)
+            var storedperson = RetrieveStoredEntity(dto);
+            if (storedperson == null)
             {
                 result = OperationStatus.NotFound;
+                result.ReasonForFailure = "No such person found";
             }
             else
             {
@@ -114,8 +107,7 @@ namespace FilmAPI.Services
         {
             var b = (BasePersonDto)dto;
             var key = _keyService.ConstructPersonKey(b.LastName, b.Birthdate);
-            var result = new KeyedPersonDto(b.LastName, b.Birthdate, b.FirstMidName, key);
-            return (IKeyedDto)result;
+            return new KeyedPersonDto(b.LastName, b.Birthdate, b.FirstMidName, key);
         }
 
         protected override Person RetrieveStoredEntity(IBaseDto dto)
